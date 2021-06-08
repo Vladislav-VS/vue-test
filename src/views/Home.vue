0c14cf495927918623e6625ce0153ed744dd338b
vlad <vlad@very-soft.com>
2021-06-08 16:53:16 +0700

    Add UsersList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ol>
+    <li v-for="user in users"
+        v-bind:user="user"
+        v-bind:key="user.id"> {{user.email}} </li>
+  </ol>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+module.exports = {
+  data()  {
+    return{
+    users:[{
+      id: 7,
+      email: "michael.lawson@reqres.in",
+      first_name: "Michael",
+      last_name: "Lawson",
+      avatar: "https://reqres.in/img/faces/7-image.jpg"
+    }, {
+      id: 8,
+      email: "lindsay.ferguson@reqres.in",
+      first_name: "Lindsay",
+      last_name: "Ferguson",
+      avatar: "https://reqres.in/img/faces/8-image.jpg"
+    }, {
+      id: 9,
+      email: "tobias.funke@reqres.in",
+      first_name: "Tobias",
+      last_name: "Funke",
+      avatar: "https://reqres.in/img/faces/9-image.jpg"
+    }, {
+      id: 10,
+      email: "byron.fields@reqres.in",
+      first_name: "Byron",
+      last_name: "Fields",
+      avatar: "https://reqres.in/img/faces/10-image.jpg"
+    }, {
+      id: 11,
+      email: "george.edwards@reqres.in",
+      first_name: "George",
+      last_name: "Edwards",
+      avatar: "https://reqres.in/img/faces/11-image.jpg"
+    }, {
+      id: 12,
+      email: "rachel.howell@reqres.in",
+      first_name: "Rachel",
+      last_name: "Howell",
+      avatar: "https://reqres.in/img/faces/12-image.jpg"
+    }]
+  }}
   }
-}
+
 </script>
+
+<style scoped>
+.foo {
+  background-color: #42b983;
+}
+</style>
