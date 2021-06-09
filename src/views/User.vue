740e72eca8cc980809dfa89a45f68fb10349a4a1
vlad <vlad@very-soft.com>
2021-06-09 12:45:08 +0700

    Cards has been made

---
+++ src/views/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contact" v-on:click="showUserEmail">
+    <div>
+      {{ user.first_name }} {{ user.last_name }}
+    </div>
+    <div v-if="showEmail">
+        {{ user.email }}
+      </div>
+    <img :src="user.avatar"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  data() {
+    return {
+      showEmail: false
+    }
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    showUserEmail() {
+      this.showEmail = !this.showEmail;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .contact{
+    background-color: antiquewhite;
+    margin-bottom: 5%;
+    border: 5px double #42b983 ;
+    padding: 3%;
+    border-radius: 40px;
+  }
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,69 @@
+<template>
+  <ol>
+    <User :user="user" v-for="user in users" :key="user.id"/>
+  </ol>
+</template>
+
+<script>
+import User from './User.vue'
+
+export default {
+  components: {
+    User
+  },
+  data() {
+    return {
+      users: [{
+        id: 7,
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+        seen: false
+      }, {
+        id: 8,
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+        seen: false
+      }, {
+        id: 9,
+        email: "tobias.funke@reqres.in",
+        first_name: "Tobias",
+        last_name: "Funke",
+        avatar: "https://reqres.in/img/faces/9-image.jpg",
+        seen: false
+      }, {
+        id: 10,
+        email: "byron.fields@reqres.in",
+        first_name: "Byron",
+        last_name: "Fields",
+        avatar: "https://reqres.in/img/faces/10-image.jpg",
+        seen: false
+      }, {
+        id: 11,
+        email: "george.edwards@reqres.in",
+        first_name: "George",
+        last_name: "Edwards",
+        avatar: "https://reqres.in/img/faces/11-image.jpg",
+        seen: false
+      }, {
+        id: 12,
+        email: "rachel.howell@reqres.in",
+        first_name: "Rachel",
+        last_name: "Howell",
+        avatar: "https://reqres.in/img/faces/12-image.jpg",
+        seen: false
+      }]
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+.foo {
+  background-color: #42b983;
+}
+</style>
